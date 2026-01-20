b71343cc1aab41b65630a091db0e99ec707d1c13
Nil Sundesha <nilsundesha@Nils-MacBook-Air.local>
2026-01-20 18:19:56 +0530

    implement dark/light theme toggle with Context API and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+import card1content from './components/CardContent';
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card image={card1content.image} title={card1content.title} description={card1content.description} price={card1content.price} buttonText={card1content.buttonText} />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/components/CardContent.js
@@ -0,0 +1,10 @@
+let image = "https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg";
+let title="ProManage – Task Tracker"
+let description = "Plan, track, and deliver projects faster with smart tools designed to improve productivity and collaboration. ProManage helps teams organize tasks, monitor progress in real time, and meet deadlines efficiently with an intuitive and user-friendly workflow."
+let price="Free Trial Available"
+let buttonText="Get Started"
+
+let card1content = {image, title, description, price, buttonText}
+
+export default card1content;
+  
